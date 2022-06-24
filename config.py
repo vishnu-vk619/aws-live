8e07d2a7d97727b2ab4683e0d00e0e91099f9e45
unknown <vishnu-vk619>
2022-06-24 10:58:24 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cujavq47oj5k.us-east-1.rds.amazonaws.com"
+customuser = "intellipaat"
+custompass = "intellipaat123"
+customdb = "employee"
+custombucket = "employee10"
+customregion = "us-east-1"
