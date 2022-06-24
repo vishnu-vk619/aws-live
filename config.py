4ab9ca38dfbbaa87e17a20818619682c4a560610
vishnu-vk619 <75382750+vishnu-vk619@users.noreply.github.com>
2022-06-24 18:31:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ customhost = "employee1.c0keurpd5p2v.us-east-1.rds.amazonaws.com"
 customuser = "intellipaat"
 custompass = "intellipaat123"
 customdb = "employee1"
-custombucket = "employee07
+custombucket = "employee07"
 customregion = "us-east-2"
